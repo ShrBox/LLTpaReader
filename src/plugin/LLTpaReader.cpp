98bb0de0999bc58512fc3ab11e9ccdf0b2b25a53
ShrBox <shrbox@outlook.com>
2024-05-18 13:14:45 +0800

    feat: add json output

--- src/plugin/LLTpaReader.cpp
+++ src/plugin/LLTpaReader.cpp
@@ -7,9 +7,12 @@
 
 #include "Homes.h"
 #include "ll/api/data/KeyValueDB.h"
+#include "ll/api/io/FileUtils.h"
 #include "ll/api/plugin/NativePlugin.h"
 #include "ll/api/plugin/Plugin.h"
 #include "ll/api/plugin/RegisterHelper.h"
+#include "nlohmann/json.hpp"
+#include "nlohmann/json_fwd.hpp"
 
 
 namespace lltpa_reader {
@@ -21,46 +24,51 @@ LLTpaReader& LLTpaReader::getInstance() { return *instance; }
 bool LLTpaReader::load() {
     getSelf().getLogger().info("Loading...");
     ll::data::KeyValueDB db(getSelf().getDataDir());
+    nlohmann::json       json;
     db.iter([&](std::string_view key, std::string_view value) {
         unsigned long long ull;
+        std::string        xuid;
         if (key.size() == sizeof(unsigned long long)) {
             std::memcpy(&ull, key.data(), sizeof(unsigned long long));
+            xuid = std::to_string(ull);
         } else {
             return true;
         }
-        getSelf().getLogger().info("{}", ull);
         Homes    homes;
         RBStream stream(value);
         homes.unpack(stream);
-        for (Homes::Home& home : homes.data) {
-            getSelf().getLogger().info("{} {}", home.name, home.pos.toStr());
+        for (auto& home : homes.data) {
+            json["homes"][xuid][home.name] = {
+                {"x",         home.pos.vc.x },
+                {"y",         home.pos.vc.y },
+                {"z",         home.pos.vc.z },
+                {"dimension", home.pos.dimid}
+            };
         }
         return true;
     });
+
     std::unordered_map<std::string, Vec4> warps;
     auto                                  warpsStr = db.get("warps");
     if (warpsStr.has_value()) {
         RBStream stream(warpsStr.value());
         stream.apply(warps);
+        for (auto& i : warps) {
+            json["warps"][i.first] = {
+                {"x",         i.second.vc.x },
+                {"y",         i.second.vc.y },
+                {"z",         i.second.vc.z },
+                {"dimension", i.second.dimid}
+            };
+        }
     }
-    for (auto& warp : warps) {
-        getSelf().getLogger().info("{} {}", warp.first, warp.second.toStr());
-    }
-    // Code for loading the plugin goes here.
+    ll::file_utils::writeFile(getSelf().getPluginDir() / "output.json", json.dump(4));
     return true;
 }
 
-bool LLTpaReader::enable() {
-    getSelf().getLogger().info("Enabling...");
-    // Code for enabling the plugin goes here.
-    return true;
-}
+bool LLTpaReader::enable() { return true; }
 
-bool LLTpaReader::disable() {
-    getSelf().getLogger().info("Disabling...");
-    // Code for disabling the plugin goes here.
-    return true;
-}
+bool LLTpaReader::disable() { return true; }
 
 } // namespace lltpa_reader
 
